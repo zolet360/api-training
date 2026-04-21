c8503e51e39deb1dae1610b6321fba3745870684
João Zolet <zolet360@gmail.com>
2026-04-21 13:16:22 -0300

    Initial commit

---
+++ src/main/java/com/expresso/api_training/ApiTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.expresso.api_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expresso/api_training/ApiTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expresso.api_training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
