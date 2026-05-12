b090cff431053d20d978bdc9f40402bd3a3d16cd
4rthurde4 <arthurdeholmes@gmail.com>
2026-05-12 20:16:50 -0300

    feat: inicio do endpoint de salvarFuncionarios

--- src/main/java/com/expresso/api_training/app/funcionario/entrypoints/resource/FuncionarioResource.java
+++ src/main/java/com/expresso/api_training/app/funcionario/entrypoints/resource/FuncionarioResource.java
@@ -3,6 +3,8 @@ package com.expresso.api_training.app.funcionario.entrypoints.resource;
 import com.expresso.api_training.commons.ResponseData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/funcionario")
@@ -10,4 +12,8 @@ public interface FuncionarioResource {
 
     @GetMapping("/teste")
     ResponseEntity<ResponseData> testeApi();
+
+    @PostMapping
+    ResponseEntity<ResponseData<FuncionarioResponse>> salvarFuncionario(@Valid @RequestBody FuncionarioRequest request);
+//o que e esse annotation de "@Valid", na verdade, o que faz cada parte do parametro dessa rota?
 }
